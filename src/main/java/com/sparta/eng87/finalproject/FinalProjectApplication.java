148b87f613a9fce6a769b2749d29ecbcbfc189b7
Tom <T-Canfield@virginmedia.com>
2021-07-15 13:36:25 +0100

    Commiting Course Type entity and Repo

--- src/main/java/com/sparta/eng87/finalproject/FinalProjectApplication.java
+++ src/main/java/com/sparta/eng87/finalproject/FinalProjectApplication.java
@@ -1,7 +1,11 @@
 package com.sparta.eng87.finalproject;
 
+import com.sparta.eng87.finalproject.entities.CourseTypeEntity;
+import com.sparta.eng87.finalproject.services.CourseTypeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinalProjectApplication {
@@ -10,4 +14,11 @@ public class FinalProjectApplication {
         SpringApplication.run(FinalProjectApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(CourseTypeService courseTypeService){
+        return (args) -> {
+          courseTypeService.save(new CourseTypeEntity("Business"));
+          courseTypeService.save(new CourseTypeEntity("Technology"));
+        };
+    }
 }
