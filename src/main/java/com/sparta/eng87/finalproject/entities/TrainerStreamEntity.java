d0f2cc693943c020ac6987cb42b7a9fe378cb41c
AlexBL <alexbarberlynch@gmail.com>
2021-07-15 14:56:11 +0100

    test

--- src/main/java/com/sparta/eng87/finalproject/entities/TrainerStreamEntity.java
+++ src/main/java/com/sparta/eng87/finalproject/entities/TrainerStreamEntity.java
@@ -3,37 +3,42 @@ package com.sparta.eng87.finalproject.entities;
 import org.hibernate.annotations.ManyToAny;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "trainer_stream", schema = "courseorganisation", catalog = "")
 public class TrainerStreamEntity {
-    private Integer trainer_id;
-    private Integer discipline_id;
+
+    @OneToMany
+    @JoinColumn(name = "trainer_id", referencedColumnName = "trainer_id")
+    private List<TraineeEntity> trainer_id;
+
+    @OneToMany
+    @JoinColumn(name = "discipline_id", referencedColumnName = "discipline_id")
+    private List<DisciplineEntity> discipline_id;
 
     public TrainerStreamEntity(){
     }
 
-    public TrainerStreamEntity(Integer trainer_id, Integer discipline_id){
+    public TrainerStreamEntity(List<TraineeEntity> trainer_id, List<DisciplineEntity> discipline_id){
         this.trainer_id = trainer_id;
         this.discipline_id = discipline_id;
     }
-    @ManyToOne
-    @JoinColumn(name = "trainer_id", referencedColumnName = "trainer_id")
-    public Integer getTrainer_id() {
+
+
+    public List<TraineeEntity> getTrainer_id() {
         return trainer_id;
     }
 
-    public void setTrainer_id(Integer trainer_id) {
+    public void setTrainer_id(List<TraineeEntity> trainer_id) {
         this.trainer_id = trainer_id;
     }
-    @ManyToOne
-    @JoinColumn(name = "discipline_id", referencedColumnName = "discipline_id")
-    public Integer getDiscipline_id() {
+
+    public List<DisciplineEntity> getDiscipline_id() {
         return discipline_id;
     }
 
-    public void setDiscipline_id(Integer discipline_id) {
+    public void setDiscipline_id(List<DisciplineEntity> discipline_id) {
         this.discipline_id = discipline_id;
     }
-
 }
