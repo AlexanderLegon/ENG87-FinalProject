28960cce00805bd1d89e1b496c69bdc78bad728f
thomas <tkirkwood@spartaglobal.com>
2021-07-15 16:45:32 +0100

    adding progress

--- src/main/java/com/sparta/eng87/finalproject/controller/FragmentController.java
+++ src/main/java/com/sparta/eng87/finalproject/controller/FragmentController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NavBarController {
+public class FragmentController {
 
-    public NavBarController() {
+    public FragmentController() {
     }
 
     @GetMapping("/")
