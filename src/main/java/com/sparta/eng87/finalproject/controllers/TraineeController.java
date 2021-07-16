f27bf848d65c91252e9743df70876bab5e4b1777
thomas <tkirkwood@spartaglobal.com>
2021-07-16 16:07:00 +0100

    adding trainee page

--- src/main/java/com/sparta/eng87/finalproject/controllers/TraineeController.java
+++ src/main/java/com/sparta/eng87/finalproject/controllers/TraineeController.java
@@ -1,13 +1,41 @@
 package com.sparta.eng87.finalproject.controllers;
 
+import com.sparta.eng87.finalproject.services.TraineeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TraineeController {
+
+    private TraineeService traineeService;
+
+    public TraineeController(TraineeService traineeService) {
+        this.traineeService = traineeService;
+    }
+
+    @Autowired
+
 
     @GetMapping("/addTrainee")
     public String getAddTraineePage() {
         return "addTrainee";
     }
+
+    @GetMapping("/traineePage/{courseName}")
+    public String displayTraineesForCourseId(@PathVariable("courseName") String courseName, Model model)
+    {
+
+        model.addAttribute("courseName", courseName);
+        model.addAttribute("trainees", traineeService.findTraineeByCourse(courseName));
+
+        return "traineePage";
+    }
+
+
+
+
+
 }
--- src/main/java/com/sparta/eng87/finalproject/repositories/CourseRepository.java
+++ src/main/java/com/sparta/eng87/finalproject/repositories/CourseRepository.java
@@ -2,9 +2,13 @@ package com.sparta.eng87.finalproject.repositories;
 
 import com.sparta.eng87.finalproject.entities.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity,Integer> {
+
+    @Query(value = "SELECT course_id FROM course WHERE course_name = ?1" ,nativeQuery = true)
+    int findCourseIdByCourseName(String courseName);
 
 }
