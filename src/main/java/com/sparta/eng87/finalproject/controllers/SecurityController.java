e8a697cbb575cf4236857177f0c451c1c18a1236
Alex Chang <alexchang0604@gmail.com>
2021-07-16 10:30:23 +0100

    made small changes

--- src/main/java/com/sparta/eng87/finalproject/controllers/SecurityController.java
+++ src/main/java/com/sparta/eng87/finalproject/controllers/SecurityController.java
@@ -1,9 +1,10 @@
 package com.sparta.eng87.finalproject.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class SecurityController {
 
     @GetMapping("/")
     public String getHomePage() {
